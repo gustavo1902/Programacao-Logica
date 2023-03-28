339f757941cf0fdade44a518740519734d83232e
Gustavo M. Costa <101591580+gustavo1902@users.noreply.github.com>
2023-03-28 12:29:26 -0300

    28/03

---
+++ Ex1.pl
@@ -0,0 +1,20 @@
+pai(joao, joaquim).
+pai(joao, lucas).
+pai(pedro, silvia).
+pai(joaquim, bruno).
+mae(silvia, bruno).
+mae(maria, lucas).
+mae(maria, joaquim).
+mae(laura, silvia).
+
+% Todos que tem filhos são felizes
+
+%São irmãos?
+irmao(X, Y) :-
+    pai(P, X), pai(P,Y),
+    mae(M, X), mae(M,Y),
+    X\=Y.
+
+neto(A, B) :-
+    pai(P, A), pai(A, B),
+    mae(M, A), mae(A, B).
