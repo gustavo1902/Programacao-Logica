964277e892bc99d07d564d44c9f032e3c228e04c
Gustavo M. Costa <101591580+gustavo1902@users.noreply.github.com>
2023-04-27 15:29:15 -0300

    27/04

---
+++ Lista1.pl
@@ -0,0 +1,10 @@
+analisa_lista([]) :-
+    write('A lista está vazia.').
+analisa_lista([Head|Tail]) :-
+    Tail = [], % lista com apenas um elemento
+    write('A cabeca da lista eh: '), write(Head), nl,
+    write('A cauda da lista eh: []').
+analisa_lista([Head|Tail]) :-
+    Tail \= [], % lista com dois ou mais elementos
+    write('A cabeca da lista eh: '), write(Head), nl,
+    write('A cauda da lista eh: '), write(Tail).
