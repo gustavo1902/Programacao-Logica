de5313d6c5b36ebad8f61e963c39488b5ef57b93
gustavo1902 <gustavomorais1009@gmail.com>
2023-05-04 14:34:10 -0300

    04/05

--- Lista1.pl
+++ Lista1.pl
@@ -1,4 +1,4 @@
-%1
+% 01º exercício
 analisa_lista([]) :-
     write('A lista está vazia.').
 analisa_lista([Head|Tail]) :-
@@ -10,8 +10,20 @@ analisa_lista([Head|Tail]) :-
     write('A cabeca da lista eh: '), write(Head), nl,
     write('A cauda da lista eh: '), write(Tail).
 
-%2
+% 02º exercício
 remove_duplicados([], []).
 remove_duplicados([H|T], [H|T1]) :- 
     delete(T, H, T2),
     remove_duplicados(T2, T1).
+
+% 03º exercício
+% 04º exercício
+% 05º exercício
+% 06º exercício
+% 07º exercício
+% 08º exercício
+% 09º exercício
+% 10º exercício
+% 11º exercício
+% 12º exercício
+% 13º exercício
