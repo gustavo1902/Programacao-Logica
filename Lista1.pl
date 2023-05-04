011c6a8dff0e3a282cf198b1d2db25ebda3d4c0f
gustavo1902 <gustavomorais1009@gmail.com>
2023-05-04 14:11:50 -0300

    04/05

--- Lista1.pl
+++ Lista1.pl
@@ -8,3 +8,5 @@ analisa_lista([Head|Tail]) :-
     Tail \= [], % lista com dois ou mais elementos
     write('A cabeca da lista eh: '), write(Head), nl,
     write('A cauda da lista eh: '), write(Tail).
+
+% 2. Escreva um predicado que recebe uma lista e retorna o último elemento da lista.
