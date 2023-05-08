4a98b02e4e76af0a86a2c3ef1fa82a90cdfec1cd
gustavo1902 <gustavomorais1009@gmail.com>
2023-05-08 07:47:56 -0300

    função_distancia

--- Lista1.pl
+++ Lista1.pl
@@ -21,18 +21,25 @@ troca([], _, _, []).
 troca([X|T], X, Y, [Y|R]) :- troca(T, X, Y, R).
 troca([H|T], X, Y, [H|R]) :- H \= X, troca(T, X, Y, R).
 
+% 04º exercício
 potencia([], [[]]).
 potencia([H|T], P) :-
     potencia(T, P1),
     findall([H|Subset], (member(Subset, P1)), NewSubsets),
     append(P1, NewSubsets, P).
 
-
-
-% 04º exercício
 % 05º exercício
+mais_longa([], [_|_]).
+mais_longa([_|_], []) :- fail.
+mais_longa([_|T1], [_|T2]) :- mais_longa(T1, T2).
+
+
 % 06º exercício
+distancia((X1,Y1), (X2,Y2), Dist) :-
+    Dist is sqrt((X2 - X1) ** 2 + (Y2 - Y1) ** 2).
+
 % 07º exercício
+
 % 08º exercício
 % 09º exercício
 % 10º exercício
