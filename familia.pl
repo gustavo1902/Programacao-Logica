8ecb44c18b01563d7cc3e23756206a0924930404
Gustavo M. Costa <101591580+gustavo1902@users.noreply.github.com>
2023-06-01 09:09:29 -0300

    correção das regras

--- familia.pl
+++ familia.pl
@@ -18,7 +18,7 @@ pai(eder, daniel).
 pai(alencar, walter).
 pai(alencar, sabrina).
 
-mae(tereza, maria)
+mae(tereza, maria).
 mae(maria, alessandra).
 mae(maria, isolda).
 mae(maria, luis).
@@ -38,7 +38,6 @@ mae(cida, walter).
 mae(cida, sabrina).
 
 
-% Regras
 sexo(tereza, feminino).
 sexo(maria, feminino).
 sexo(luzia, feminino).
@@ -64,34 +63,22 @@ sexo(daniel, masculino).
 sexo(alencar, masculino).
 sexo(walter, masculino).
 
-irmao(X, Y) :- pai(Z, X), pai(Z, Y), X \== Y, sexo(X, masculino).
-irma(X, Y) :- mae(Z, X), mae(Z, Y), X \== Y, sexo(X, feminino).
+%Regra
+
+/*Considerei apenas parentes diretos
+Pelo fato de todos os irmaõs terem o mesmo pai e mãe coloquei apenas essa regra.*/
 irmaos(X, Y) :- pai(Z, X), pai(Z, Y), X \== Y.
-
-mae(X, Y) :- mae(X, Y), sexo(X, feminino).
-pai(X, Y) :- pai(X, Y), sexo(X, masculino).
-
-filho(X, Y) :- pai(Y, X), sexo(X, masculino).
-filha(X, Y) :- pai(Y, X), sexo(X, feminino).
-filhos(X, Y) :- pai(Y, X).
-
-avo(X, Y) :- pai(X, Z), pai(Z, Y), sexo(X, masculino).
-avoh(X, Y) :- mae(X, Z), mae(Z, Y), sexo(X, feminino).
+ 
 avos(X, Y) :- pai(X, Z), pai(Z, Y).
+avos(X, Y) :- mae(X, Z), pai(Z, Y).
+avos(X, Y) :- pai(X, Z), mae(Z, Y).
+avos(X, Y) :- mae(X, Z), mae(Z, Y).
 
-tio(X, Y) :- pai(Z, Y), irmao(X, Z).
-tia(X, Y) :- mae(Z, Y), irma(X, Z).
 tios(X, Y) :- pai(Z, Y), irmaos(X, Z).
+tios(X, Y) :- mae(Z, Y), irmaos(X, Z).
 
-primo(X, Y) :- pai(Z, X), pai(W, Y), irmaos(Z, W), sexo(X, masculino).
-prima(X, Y) :- pai(Z, X), pai(W, Y), irmaos(Z, W), sexo(X, feminino).
-primos(X, Y) :- pai(Z, X), pai(W, Y), irmaos(Z, W).
+primos(X, Y) :- (mae(Z, X), mae(W, Y), irmaos(Z, W) ; pai(Z, X), pai(W, Y), irmaos(Z, W) ; mae(Z, X), pai(W, Y), irmaos(Z, W) ; pai(Z, X), mae(W, Y), irmaos(Z, W)).
 
-sobrinho(X, Y) :- pai(Z, X), pai(W, Y), irmaos(Z, W), sexo(X, masculino).
-sobrinha(X, Y) :- pai(Z, X), pai(W, Y), irmaos(Z, W), sexo(X, feminino).
-sobrinhos(X, Y) :- pai(Z, X), pai(W, Y), irmaos(Z, W).
+sobrinhos(X, Y) :- (pai(Z, X), irmaos(Z, Y) ; mae(Z, X), irmaos(Z, Y)).
 
-neto(X, Y) :- pai(Y, Z), pai(Z, X), sexo(X, masculino).
-neta(X, Y) :- pai(Y, Z), pai(Z, X), sexo(X, feminino).
-netos(X, Y) :- pai(Y, Z), pai(Z, X).
-
+netos(X, Y) :- (pai(Y, Z), pai(Z, X)) ; (pai(Y, Z), mae(Z, X)) ; (mae(Y, Z), pai(Z, X)) ; (mae(Y, Z), mae(Z, X)).
